6ecd49e8a7253b58eeb0f0985b43b0d6c121210f
ThuanLG <hackmuine456@gmail.com>
2018-03-11 18:55:25 +0700

    Testing some function

---
+++ API/Controllers/ThiefController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class ThiefController : Controller
+    {
+        // GET: api/Thief
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET: api/Thief/5
+        [HttpGet("/api/[controller]/[action]/hamtruyen/{url}")]
+        public IActionResult Get(string url)
+        {
+
+            return Ok(url);
+        }
+        
+    }
+}
---
+++ TestFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+
+namespace TestFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var domain = "https://hamtruyen.com";
+            var url = "https://hamtruyen.com/doc-truyen/nguyen-muc-chapter-179.html";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            //var alink = doc.DocumentNode.Descendants("a").Where(e => e.ParentNode.Attributes["class"] != null && e.ParentNode.Attributes["class"].Value == "col_chap tenChapter");
+            var imglink= doc.DocumentNode.Descendants("img")
+                            .Where(e => 
+                                e.ParentNode.Attributes["class"] != null 
+                                && 
+                                e.ParentNode.Attributes["class"].Value == "content_chap");
+            imglink.ToList().ForEach(e => Console.WriteLine(e.Attributes["src"].Value));
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
